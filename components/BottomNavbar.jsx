566cd0a120b45ad27c63c1f2d5b514593efc5e0e
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-02-28 21:45:38 +1000

    structure bottom nav

---
+++ components/BottomNavbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const BottomNavbar = () => {
+  return (
+    <div className="nav__menu" id="nav-menu">
+    <ul className="nav__list">
+        <li className="nav__item">
+            <a href="#home" className="nav__link active-link">
+                <i className='bx bx-home-alt nav__icon'></i>
+                <span className="nav__name">Home</span>
+            </a>
+        </li>
+        
+        <li className="nav__item">
+            <a href="#about" className="nav__link">
+                <i className='bx bx-user nav__icon'></i>
+                <span className="nav__name">About</span>
+            </a>
+        </li>
+
+        <li className="nav__item">
+            <a href="#skills" className="nav__link">
+                <i className='bx bx-book-alt nav__icon'></i>
+                <span className="nav__name">Skills</span>
+            </a>
+        </li>
+
+        <li className="nav__item">
+            <a href="#portfolio" className="nav__link">
+                <i className='bx bx-briefcase-alt nav__icon'></i>
+                <span className="nav__name">Portfolio</span>
+            </a>
+        </li>
+
+        <li className="nav__item">
+            <a href="#contactme" className="nav__link">
+                <i className='bx bx-message-square-detail nav__icon'></i>
+                <span className="nav__name">Contactme</span>
+            </a>
+        </li>
+    </ul>
+</div>
+  )
+}
+
+export default BottomNavbar
