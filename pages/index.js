41c7508192ab904847a24261d1cef5ab9c472386
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-03-05 23:32:33 +1000

    Home page sequence explained

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
+// This is the intro page. Itll will begin with a fullscreen display of SquadLink Logo (with a slow & sutble video background)-- 
+// The logo should prompt the user to tap/click -- 
+// upon actuation, there will be an animation which renders the sign in form (opacity fade in & slide up )-- 
 
+// once the user enters their details the entire page will fade out & redirect them to the next page (animated loading icon whilst page loads)
+// (registering new user ? take them to registration / account set up page : sign in )
+        
 
 
 export default function Home() {
   return (
       <>
-          this is some words
+
       </>
   )
 }
