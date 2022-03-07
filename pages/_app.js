c1abe4c74d92c55b52e4ce7190dbeb84e021455e
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-03-07 19:01:45 +1000

    Component stylesheets successfully linked

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
+// import global stylesheet
+// import each css file from each component here
 import '../globals.css'
 import '../src/components/BottomNav/BottomNav.css'
+import '../src/components/TopNav/TopNav.css'
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
+// import necessary components 
+import { TopNav, BottomNav } from '../src/components';
+
 // This is the intro page. Itll will begin with a fullscreen display of SquadLink Logo (with a slow & sutble video background)-- 
 // The logo should prompt the user to tap/click (the logo) -- 
 // upon actuation, there will be an animation which renders the sign in form (opacity fade in & slide up )-- 
-
-import { BottomNav } from '../src/components';
-import { TopNav } from '../src/components';
 
 // once the user enters their details the entire page will fade out & redirect them to the next page (animated loading icon whilst page loads)
 // (registering new user ? take them to registration / account set up page : sign in )
