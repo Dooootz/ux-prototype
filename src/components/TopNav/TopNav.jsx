9e9d56a30dafa21b84b6dec9ec36cb7e0a39a1e0
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-03-07 19:17:25 +1000

    Link successful

--- src/components/TopNav/TopNav.jsx
+++ src/components/TopNav/TopNav.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 
 const TopNav = () => {
   return (
-    <div className='top-nav'>TopNav</div>
+    <div class="top-nav d-flex justify-content-between p-3 align-items-center">
+      <i onclick="menuNav()" class="bi bi-arrow-left-circle"></i>
+        <div class="text-center profile-status">
+          <h5 class="pt-3">CHAT</h5>
+          <p>4 Unread Messages</p>
+        </div>
+      <i onclick="settings()" class="bi bi-gear-wide-connected"></i>
+    </div>
   )
 }
 
