0715c103f8120ea1e1c4e13f41436a70efc5aace
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-03-19 20:24:03 +1000

    changed to jsx syntax

--- src/components/TopNav/TopNav.jsx
+++ src/components/TopNav/TopNav.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 
 const TopNav = () => {
   return (
-    <div class="top-nav d-flex justify-content-between p-3 align-items-center">
-      <i onclick="menuNav()" class="bi bi-arrow-left-circle"></i>
-        <div class="text-center profile-status">
-          <h5 class="pt-3">CHAT</h5>
+    <div className="top-nav d-flex justify-content-between p-3 align-items-center">
+      <i onclick="menuNav()" className="bi bi-arrow-left-circle"></i>
+        <div className="text-center profile-status">
+          <h5 className="pt-3">CHAT</h5>
           <p>4 Unread Messages</p>
         </div>
-      <i onclick="settings()" class="bi bi-gear-wide-connected"></i>
+      <i onclick="settings()" className="bi bi-gear-wide-connected"></i>
     </div>
   )
 }
